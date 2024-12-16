b8f3ef00a067429dbc779cec61c0d35820f6228a
Edgar Lipnevic <edgarlipnevic@gmail.com>
2024-12-16 19:26:58 +0200

    JS folder creation and questions array added

---
+++ scripts/questions.js
@@ -0,0 +1,59 @@
+export const questions = [
+  {
+    question: "What does HTML stand for?",
+    choices: [
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+      "Hyperlinks and Text Markup Language",
+      "Hyper Text Makeup Language",
+    ],
+    correctAnswerIndex: 0,
+  },
+  {
+    question: "Which language is used for styling web pages?",
+    choices: ["HTML", "CSS", "JavaScript", "Python"],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: "What does CSS stand for?",
+    choices: [
+      "Cascading Style Sheets",
+      "Creative Style Sheets",
+      "Computer Style Sheets",
+      "Colorful Style Sheets",
+    ],
+    correctAnswerIndex: 0,
+  },
+  {
+    question: "Which language runs in a web browser?",
+    choices: ["Java", "C", "Python", "JavaScript"],
+    correctAnswerIndex: 3,
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script in HTML?",
+    choices: [
+      "<script src='script.js'>",
+      "<script link='script.js'>",
+      "<script href='script.js'>",
+      "<script name='script.js'>",
+    ],
+    correctAnswerIndex: 0,
+  },
+  {
+    question:
+      "What is the main purpose of the `<head>` section in an HTML document?",
+    choices: [
+      "To contain visible content of the webpage",
+      "To specify metadata and link resources like stylesheets",
+      "To structure the body of the webpage",
+      "To display the title of the document",
+    ],
+    correctAnswerIndex: 1,
+  },
+  {
+    question: "Which of the following is not a JavaScript data type?",
+    choices: ["String", "Boolean", "Alert", "Number"],
+    correctAnswerIndex: 2,
+  },
+];
